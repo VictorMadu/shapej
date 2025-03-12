f43148d95e04230a806269e7000157078f92f255
Victor Ebube Madu <ebubevm@gmail.com>
2025-03-12 16:06:26 +0100

    chore: project set up, feat: define circles and rectangles

---
+++ geometry/src/main/java/io/victormadu/github/geometry/Circle.java
@@ -0,0 +1,5 @@
+package io.victormadu.github.geometry;
+
+public interface Circle extends Shape {
+    double raidus();
+}
---
+++ geometry/src/main/java/io/victormadu/github/geometry/Rectangle.java
@@ -0,0 +1,9 @@
+package io.victormadu.github.geometry;
+
+public interface Rectangle extends Shape{
+    double length();
+    double width();
+} 
+
+
+    
